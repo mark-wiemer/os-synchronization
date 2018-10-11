c4e4dd1e0ccddf5137aac6f2452e3cb3636b5f01
Mark Wiemer <markwiemer@outlook.com>
2018-10-11 15:01:00 -0500

    Add pthread library

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 // Mark Wiemer mww 9074356420
 // Jenny Ye haengjung 9075878315
 
+#include <pthread.h>
+
 int main() {
+	// Create the threads
+	pthread_t reader_t;
+
+	// Create the queues
+
+	// Start the threads
+
+	// Wait for the threads to terminate
+
 	return 0;
 }
